7aa6387f899941676d1046194e54a8c28939b90a
Miguel duarte <ms1169929@gmail.com>
2026-09-14 16:21:49 -0300

    miguelzin delas

---
+++ projetobiblioteca/src/main/java/br/com/senac/projetobiblioteca/Projetobiblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.projetobiblioteca;
+
+/**
+ *
+ * @author miguel62977036
+ */
+public class Projetobiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
